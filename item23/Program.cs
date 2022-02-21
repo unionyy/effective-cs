803220d9896492b91bd5e53cb08c6a796e9b7615
unionyy <jadu1028@naver.com>
2022-02-22 01:26:01 +0900

    add item23

---
+++ item23/Program.cs
@@ -0,0 +1,29 @@
+﻿MyClass a = new MyClass(1);
+MyClass b = new MyClass(2);
+Example.Add<MyClass>(a, b);
+
+public class MyClass : IAdd<MyClass>
+{
+    int value;
+    public MyClass(int value)
+    {
+        this.value = value;
+    }
+    public MyClass AddFunc(MyClass A)
+    {
+        return new MyClass(this.value + A.value);
+    }
+}
+// AddFunc 메서드를 사용하 위해IAdd<T> 인터페이스 선언
+public interface IAdd<T>
+{
+	T AddFunc(T A);
+}
+
+public static class Example
+{
+	public static T Add<T>(T left, T right) where T : IAdd<T>
+    {
+        return left.AddFunc(right);
+    }
+}
