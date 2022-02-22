754530765f8afbba8a34b11ef219183a7dbc2b47
unionyy <jadu1028@naver.com>
2022-02-22 19:05:09 +0900

    item27

---
+++ item27/Program.cs
@@ -0,0 +1,26 @@
+﻿using FooEx;
+
+MyType t = new MyType();
+t.Marker = 1;
+t.NextMarker();
+Console.WriteLine(t.Marker);
+
+public interface IFoo
+{
+    int Marker { get; set; }
+}
+
+public class MyType : IFoo
+{
+    public int Marker { get; set; }
+}
+
+
+namespace FooEx
+{
+    public static class FooExtenstions
+    {
+        public static void NextMarker(this IFoo thing) =>
+            thing.Marker += 1;
+    }
+}
